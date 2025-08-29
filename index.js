99f77da87df3a55716db3ccd1accd511286f2f44
Deepika Anumula <anumuladeepika99@gmail.com>
2025-08-29 14:19:56 +0530

    Update CORS to allow localhost and live domain

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 // CORS Setup – allow only your frontend domain
 app.use(cors({
-  origin: 'https://ssjmatha.in',  // ✅ Use your actual frontend domain
+  origin: ['http://localhost:3000', 'https://ssjmatha.in'], // ✅ Use your actual frontend domain
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
